f302f19efacda857805ec14c018d2be6cc1c53b3
carefreetime <carefreetime1224@gmail.com>
2018-05-15 23:08:48 +0800

    add getbulk

--- service_script.js
+++ service_script.js
@@ -2,6 +2,7 @@ var App = (function () {
     function _bindEvent() {
         console.log('bind event');
         $('#get_submit').on('click', _getsomething);
+        $('#getbulk_submit').on('click', _getbulk);
         $('#refresh').on('click', _refresh);
     }
 
@@ -174,14 +175,58 @@ var App = (function () {
         });
     }
 
-    function _getDefault(oid, id) {
+    function _getbulk() {
+        $('#table').removeClass('hidden_text');
+        $('#tbody').html("");
+
+        var n = $('#n').val();
+        var m = $('#m').val();
+        var oid1 = $('#oid1').val();
+        var oid2 = $('#oid2').val();
+
+        $.ajax ({
+            url : `http://163.22.32.174:4000/getbulk/` + n + `/` + m + `/` + oid1 + `/` + oid2,
+            type : 'get',
+            dataType : 'json',
+            success : function(data) {
+                for(let objects of data) {
+                    var oid = objects.oid;
+                    var type = objects.type;
+                    var value = JSON.stringify(objects.value);
+
+                    if(value == '""') {
+                        $('#tbody').append(`
+                            <tr>
+                                <td bgcolor="#AAAAAA">`+oid+`</td>
+                                <td bgcolor="#AAAAAA">`+type+`</td>
+                                <td bgcolor="#AAAAAA"></td>
+                            </tr>
+                        `);
+                    } else {
+                        $('#tbody').append(`
+                            <tr>
+                                <td>`+oid+`</td>
+                                <td>`+type+`</td>
+                                <td>`+value+`</td>
+                            </tr>
+                        `);
+                    }    
+                }   
+            },
+            error : function(jqXHR) {
+                console.log(jqXHR);
+            }
+        });
+    }
+
+    function _getDefault(oid, idname) {
         $.ajax({
             url : `http://163.22.32.174:4000/get/` + oid,
             type : 'get',
             dataType : 'json',
             success : function(data) {
                 var value = JSON.stringify(data.value);
-                $('#' + id).val(value);
+                $('#' + idname).val(value);
             },
             error : function(jqXHR) {
                 console.log(jqXHR);
