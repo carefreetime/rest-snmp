03bbc982dba22e7accd02919901afaa83b64656f
carefreetime <carefreetime1224@gmail.com>
2018-06-19 23:31:08 +0800

    push trap receiver into web server

--- rest-snmp.js
+++ rest-snmp.js
@@ -4,6 +4,16 @@ const cors = require('cors');
 const session = require('express-session');
 const mongoose = require('mongoose');
 const MongoStore = require('connect-mongo')(session);
+var snmp = require('snmpjs');
+var bunyan = require('bunyan');
+var util = require('util');
+var Trap = require('./models/trap');
+var dateTime = require('node-datetime');
+
+// set up express app
+const app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 //connect to MongoDB
 mongoose.connect('mongodb://localhost/rest_snmp');
@@ -14,9 +24,6 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
   // we're connected!
 });
-
-// set up express app
-const app = express();
 
 //use sessions for tracking logins
 app.use(session({
@@ -43,13 +50,77 @@ app.use('/', require('./routes/api'));
 // serve static files from template
 app.use(express.static(__dirname + '/'));
 
+app.get('/trap', function(req, res){
+  res.sendFile(__dirname + '/gettrap.html');
+});
+
 //error handling middleware
 app.use(function (err, req, res, next) {
   // console.log(err);
   res.status(422).send({error:err.message});
 });
 
+var options = {
+  addr: '163.22.32.174',
+  port: 161,
+  family: 'udp4',
+};
+
+var log = new bunyan({ name: 'snmpd', level: 'trace'});
+
+var trapd = snmp.createTrapListener({log: log});
+
+var dt = dateTime.create();
+var formatted = dt.format('Y-m-d H:M:S');
+
+trapd.on('trap',function(msg) {
+  // console.log(msg);
+  // console.log('-------------------------------------------------');
+  // console.log(util.inspect(snmp.message.serializer(msg), false, null, true));
+  // console.log('-=================================-');
+  // console.log(util.inspect(snmp.message.serializer(msg)));
+  // console.log(util.inspect(snmp.message.serializer(msg).pdu.varbinds));
+  // console.log('-=================================-');  
+  io.emit('chat message', util.inspect(snmp.message.serializer(msg), false, null));
+  var traps = snmp.message.serializer(msg).pdu.varbinds;
+  for (let object of traps) {
+      var trapData = {
+          version : snmp.message.serializer(msg).version,
+          community : snmp.message.serializer(msg).community,
+          time : formatted,
+          pdu : {
+              op : snmp.message.serializer(msg).pdu.op,
+              enterprise : snmp.message.serializer(msg).pdu.enterprise,
+              agent_addr : snmp.message.serializer(msg).pdu.agent_addr,
+              generic_trap : snmp.message.serializer(msg).pdu.generic_trap,
+              specific_trap : snmp.message.serializer(msg).pdu.specific_trap,
+              time_stamp : snmp.message.serializer(msg).pdu.time_stamp,
+              varbinds : {
+                  oid : object.oid,
+                  typename : object.typename,
+                  value : object.value,
+                  string_value : object.string_value 
+              }
+          }      
+      }      
+      trapcreate(trapData);    
+      io.emit('chat message', trapData);              
+  }
+});
+
+function trapcreate(trapData) {
+  Trap.create(trapData, function (error, trap) {
+      if (error) {
+          console.log(error);
+      } else {
+          console.log(trapData);
+      }
+  });
+}
+
+trapd.bind(options);
+
 //listen for requests
-app.listen(process.env.port || 4000, function () {
+http.listen(process.env.port || 4000, function () {
   console.log('now listening for requests');
 });
