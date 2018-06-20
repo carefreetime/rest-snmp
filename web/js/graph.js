392a4f8043d1f72fe8ac0b34af4145320bb73de9
carefreetime <carefreetime1224@gmail.com>
2018-06-21 01:40:44 +0800

    arrange web files

---
+++ web/js/graph.js
@@ -0,0 +1,157 @@
+var App = (function () {
+    function _bindEvent() {
+        console.log('bind event');
+        google.charts.load('current', {'packages':['line']});
+        google.charts.setOnLoadCallback(drawChart);
+        $('#serviceId').on('change', _serviceChange);
+        $('#selectId').on('change', _selectChange);
+        $('#start').on('click', _graph);
+        $('#stop').on('click', _stopGraph);
+        $('#clear').on('click', _clearGraph);
+    }
+
+    var serviceId = 0;
+    var selectId = 0;
+    var sec = 0;
+    var service;
+    var getifInOctets, getifOutOctets;
+
+    function _graph() {
+        sec = $('#sec').val();
+        if (serviceId == 1) {
+            service = 'ifInOctets';
+            getifInOctets = setInterval(_getifInOctets, sec * 1000);
+        } else {
+            service = 'ifOutOctets';            
+            getifOutOctets = setInterval(_getifOutOctets, sec * 1000);
+        }        
+    }
+
+    function _stopGraph() {
+        clearInterval(getifInOctets);
+        clearInterval(getifOutOctets);
+    }
+
+    function _clearGraph() {
+        _stopGraph();
+        ifOctets = [];
+        drawChart();
+    }
+
+    function _getifNumber() {
+        var selectId = $('selectId').val();
+        $.ajax({
+            url : `http://163.22.32.174:4000/get/1.3.6.1.2.1.2.1.0/`,
+            type : 'get',
+            dataType : 'json',
+            contentType: 'application/json', 
+			xhrFields: {
+				withCredentials: true
+			},
+            success : function(data) {
+                var value = data.value;
+                for (var i = 1; i <= value; i++) {
+                    $('#selectId').append(`
+                        <option value="` + i + `">` + i + `</option>
+                    `);
+                }                
+            },
+            error : function(jqXHR) {
+                console.log(jqXHR);
+            }
+        });
+    }
+
+    function _serviceChange() {
+        var selectObj = document.getElementById("serviceId");
+        serviceId = selectObj.options[selectObj.options.selectedIndex].value;
+        _getifNumber()
+    }
+
+    function _selectChange() {
+        var selectObj = document.getElementById("selectId");
+        selectId = selectObj.options[selectObj.options.selectedIndex].value;
+    }
+
+    var ifOctets = [];
+    var date = [];
+    var tmp = 0;
+
+    function drawChart() {
+        // Create the data table.
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Times');
+        data.addColumn('number', service);
+
+        // 計算流量的公式
+        // (ifInOctetsCurrent - ifInOctetsPrevious) * 8 / pollingSeconds
+        for (var i = 0; i <= ifOctets.length; i++) {
+            data.addRows([[date[i], (parseFloat(ifOctets[i]) - tmp) * 8 / sec]]);
+            tmp =  parseFloat(ifOctets[i]);          
+        }
+
+        // Set chart options
+        var options = {
+            chart: {
+                title: service,
+            },
+            width: "100%",
+            height: 500
+        };
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.charts.Line(document.getElementById('linechart_material'));
+        chart.draw(data, google.charts.Line.convertOptions(options));
+    }
+
+    function _getifInOctets() {
+        $.ajax({
+            url : `http://163.22.32.174:4000/get/1.3.6.1.2.1.2.2.1.10.` + selectId + `/`,
+            type : 'get',
+            dataType : 'json',
+            contentType: 'application/json', 
+			xhrFields: {
+				withCredentials: true
+			},
+            success : function(data) {
+                var value = data.value;                
+                ifOctets.push(value); 
+                date.push(new Date().toLocaleTimeString());
+                drawChart();       
+            },
+            error : function(jqXHR) {
+                console.log(jqXHR);
+            }
+        });
+    }
+
+    function _getifOutOctets() {
+        $.ajax({
+            url : `http://163.22.32.174:4000/get/1.3.6.1.2.1.2.2.1.16.` + selectId + `/`,
+            type : 'get',
+            dataType : 'json',
+            contentType: 'application/json', 
+			xhrFields: {
+				withCredentials: true
+			},
+            success : function(data) {
+                var value = data.value;                
+                ifOctets.push(value); 
+                date.push(new Date().toLocaleTimeString());
+                drawChart();       
+            },
+            error : function(jqXHR) {
+                console.log(jqXHR);
+            }
+        });
+    }
+    
+    function init() {
+        console.log('Hello');
+        _bindEvent();
+    }
+
+    return {
+        init
+    }
+})();
---
+++ web/js/service.js
@@ -0,0 +1,332 @@
+var App = (function () {
+    function _bindEvent() {
+        console.log('bind event');
+        $('#get_submit').on('click', _getsomething);
+        $('#getbulk_submit').on('click', _getbulk);
+        $('#set_submit').on('click', _set);
+        $('#selectId').on('change', PDUOnChange);
+        $('#n').on('change', NOnChange);
+    }
+
+    function _getsomething() {
+        $('#table').removeClass('hidden_text');
+        $('#tbody').html("");
+
+        var service = $('#selectId').val();
+        switch(service) {
+            case '1' :
+                _get();
+                break;
+            case '2' :
+                _getNext();
+                break;
+            case '4' :
+                _getSubTree();
+                break;
+            case '5' :
+                _walk();
+                break;
+        }
+    }
+
+    function _get() {
+        var oid = $('#oid').val();
+        $.ajax({
+            url : `http://163.22.32.174:4000/get/` + oid,
+            type : 'get',
+            dataType : 'json',
+            success : function(data) {
+                var oid = data.oid;
+                var type = data.type;
+                var value = JSON.stringify(data.value);
+
+                if(value == '""') {
+                    $('#tbody').append(`
+                        <tr>
+                            <td bgcolor="#AAAAAA">`+oid+`</td>
+                            <td bgcolor="#AAAAAA">`+type+`</td>
+                            <td bgcolor="#AAAAAA"></td>
+                        </tr>
+                    `);
+                } else {
+                    $('#tbody').append(`
+                        <tr>
+                            <td>`+oid+`</td>
+                            <td>`+type+`</td>
+                            <td>`+value+`</td>
+                        </tr>
+                    `);
+                }              
+            },
+            error : function(jqXHR) {
+                console.log(jqXHR);
+            }
+        });
+    }
+
+    function _getNext() {
+        var oid = $('#oid').val();
+        $.ajax({
+            url : `http://163.22.32.174:4000/getnext/` + oid,
+            type : 'get',
+            dataType : 'json',
+            success : function(data) {
+                var oid = data.oid;
+                var type = data.type;
+                var value = JSON.stringify(data.value);
+
+                if(value == '""') {
+                    $('#tbody').append(`
+                        <tr>
+                            <td bgcolor="#AAAAAA">`+oid+`</td>
+                            <td bgcolor="#AAAAAA">`+type+`</td>
+                            <td bgcolor="#AAAAAA"></td>
+                        </tr>
+                    `);
+                } else {
+                    $('#tbody').append(`
+                        <tr>
+                            <td>`+oid+`</td>
+                            <td>`+type+`</td>
+                            <td>`+value+`</td>
+                        </tr>
+                    `);
+                } 
+            },
+            error : function(jqXHR) {
+                console.log(jqXHR);
+            }
+        });
+    }
+
+    function _getSubTree() {
+        var oid = $('#oid').val();
+        $.ajax({
+            url : `http://163.22.32.174:4000/subtree/` + oid,
+            type : 'get',
+            dataType : 'json',
+            success : function(data) {
+                for(let objects of data) {
+                    var oid = objects.oid;
+                    var type = objects.type;
+                    var value = JSON.stringify(objects.value);
+
+                    if(value == '""') {
+                        $('#tbody').append(`
+                            <tr>
+                                <td bgcolor="#AAAAAA">`+oid+`</td>
+                                <td bgcolor="#AAAAAA">`+type+`</td>
+                                <td bgcolor="#AAAAAA"></td>
+                            </tr>
+                        `);
+                    } else {
+                        $('#tbody').append(`
+                            <tr>
+                                <td>`+oid+`</td>
+                                <td>`+type+`</td>
+                                <td>`+value+`</td>
+                            </tr>
+                        `);
+                    } 
+                }                  
+            },
+            error : function(jqXHR) {
+                console.log(jqXHR);
+            }
+        });
+    }
+
+    function _walk() {
+        var oid = $('#oid').val();
+        $.ajax({
+            url : `http://163.22.32.174:4000/walk/` + oid,
+            type : 'get',
+            dataType : 'json',
+            success : function(data) {
+                for(let objects of data) {
+                    var oid = objects.oid;
+                    var type = objects.type;
+                    var value = JSON.stringify(objects.value);
+
+                    if(value == '""') {
+                        $('#tbody').append(`
+                            <tr>
+                                <td bgcolor="#AAAAAA">`+oid+`</td>
+                                <td bgcolor="#AAAAAA">`+type+`</td>
+                                <td bgcolor="#AAAAAA"></td>
+                            </tr>
+                        `);
+                    } else {
+                        $('#tbody').append(`
+                            <tr>
+                                <td>`+oid+`</td>
+                                <td>`+type+`</td>
+                                <td>`+value+`</td>
+                            </tr>
+                        `);
+                    }    
+                }   
+            },
+            error : function(jqXHR) {
+                console.log(jqXHR);
+            }
+        });
+    }
+
+    function _getbulk() {
+        $('#table').removeClass('hidden_text');
+        $('#tbody').html("");
+
+        var n = $('#n').val();
+        var m = $('#m').val();
+        
+        var oids = '';
+        for (var i = 1; i <= n; i++) {
+            oids += ($('#oid' + i).val()) + ',';
+        }    
+        oids += ($('#oid' + (parseInt(n) + 1)).val());
+
+        $.ajax ({
+            url : `http://163.22.32.174:4000/getbulk/` + n + `/` + m + `/` + oids,
+            type : 'get',
+            dataType : 'json',
+            success : function(data) {
+                for(let objects of data) {
+                    var oid = objects.oid;
+                    var type = objects.type;
+                    var value = JSON.stringify(objects.value);
+
+                    if(value == '""') {
+                        $('#tbody').append(`
+                            <tr>
+                                <td bgcolor="#AAAAAA">`+oid+`</td>
+                                <td bgcolor="#AAAAAA">`+type+`</td>
+                                <td bgcolor="#AAAAAA"></td>
+                            </tr>
+                        `);
+                    } else {
+                        $('#tbody').append(`
+                            <tr>
+                                <td>`+oid+`</td>
+                                <td>`+type+`</td>
+                                <td>`+value+`</td>
+                            </tr>
+                        `);
+                    }    
+                }   
+            },
+            error : function(jqXHR) {
+                console.log(jqXHR);
+            }
+        });
+    }
+
+    function _set() {
+        $('#table').removeClass('hidden_text');
+        $('#tbody').html("");
+        
+        var oid = $('#oid0').val();
+        var type = $('#type').val();
+        var value = $('#value').val();
+
+        $.ajax({
+            url : `http://163.22.32.174:4000/set/` + oid + `/` + type + `/` + value,
+            type : 'put',
+            dataType : 'json',
+            data : JSON.stringify({
+                oid : $('#oid').val(),
+                type : $('#type').val(),
+                value : $('#value').val()
+            }),
+            success : function(data) {
+                console.log('success');
+                if(value == '""') {
+                    $('#tbody').append(`
+                        <tr>
+                            <td bgcolor="#AAAAAA">`+oid+`</td>
+                            <td bgcolor="#AAAAAA">`+type+`</td>
+                            <td bgcolor="#AAAAAA"></td>
+                        </tr>
+                    `);
+                } else {
+                    $('#tbody').append(`
+                        <tr>
+                            <td>`+oid+`</td>
+                            <td>`+type+`</td>
+                            <td>`+value+`</td>
+                        </tr>
+                    `);
+                }
+            },
+            error : function(jqXHR) {
+                console.log(jqXHR);
+            }
+        });
+    }
+
+    function ChangeClass(obj) {
+        var selectObj = document.getElementById(obj);
+        if (selectObj.options[selectObj.options.selectedIndex].value == 3) {
+            $('#tbody').html("");
+            $("#table").addClass("hidden_text");
+            $("#get").addClass("hidden_text");
+            $("#getbulk").removeClass("hidden_text");
+            $("#set").addClass("hidden_text");
+        } else if (selectObj.options[selectObj.options.selectedIndex].value == 6) {
+            $('#tbody').html("");
+            $("#table").addClass("hidden_text");
+            $("#get").addClass("hidden_text");
+            $("#getbulk").addClass("hidden_text");
+            $("#set").removeClass("hidden_text");
+        } else if (selectObj.options[selectObj.options.selectedIndex].value == 0) {
+            $('#tbody').html("");
+            $("#table").addClass("hidden_text");
+            $("#table").addClass("hidden_text");
+            $("#get").addClass("hidden_text");
+            $("#getbulk").addClass("hidden_text");
+            $("#set").addClass("hidden_text");
+        } else {
+            $('#tbody').html("");
+            $("#table").addClass("hidden_text");
+            $("#get").removeClass("hidden_text");
+            $("#getbulk").addClass("hidden_text");
+            $("#set").addClass("hidden_text");
+        }
+    }
+
+    function PDUOnChange() {
+        ChangeClass("selectId");
+    }
+
+    function NOnChange() {
+        $('#getbulk_oids').html("");
+        $("#getbulk_button").removeClass("hidden_text");
+
+        var n = document.getElementById("n").value;
+        for (var i = 1; i <= n; i++) {
+            $('#getbulk_oids').append(`
+                <label for="oid" class="col-md-1 col-form-label text-right">oid`+i+`</label>
+                <div class="col-md-3">
+                    <input type="text" class="form-control" id="oid`+i+`">
+                </div>                
+            `);
+        }
+
+        var m = ++n;
+        $('#getbulk_oids').append(`
+            <label for="oid" class="col-md-1 col-form-label text-right">oid`+m+`</label>
+            <div class="col-md-3">
+                <input type="text" class="form-control" id="oid`+m+`">
+            </div>
+        `)
+    }
+
+    function init() {
+        console.log('Hello');
+        _bindEvent();
+    }
+
+    return {
+        init
+    }
+})();
